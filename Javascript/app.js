c4fc6f43d80142f24ee7fe026abad9a49f84cb11
PepijnTEW <37923@ma-web.nl>
2024-07-22 01:25:48 +0200

    03 draw board

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,8 +1,8 @@
 "use strict";
 
-//Cavas Variables
-let cavas = document.getElementById("cavas");
-let g = cavas.getContext("2d");
+//Canvas Variables
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");
 
 //Gamestate Variables
 const GAMESTATE_START = 0;
@@ -18,3 +18,65 @@ let boardPositionSize = 50;
 let pawnPositions = [];
 let boardPositions = [];
 let playerAmountButtons = [];
+
+//Ractangle Function
+function createRect(x,y,w,h)
+{
+    let rectangle = {
+        x:x,
+        y:y,
+        x2:x+w,
+        y2:y+h,
+        w:w,
+        h:h,
+    };
+    return rectangle;
+}
+
+function clearCanvas()
+{
+    g.fillStyle = "lightslategray";
+    g.fillRect(0,0,canvas.with, canvas.height);
+}
+
+function draw()
+{
+    clearCanvas();
+    for (let i = 0; i < boardPositions.length; i++) {
+        let pos = boardPositions[i];
+
+        g.fillStyle = "#004400";
+        g.fillRect(pos.x,pos.y,50,50);
+        g.fillStyle = "#FFFFFF";
+        g.fillText((i+1)+"",pos.x,pos.y+20);
+        
+    }
+}
+
+function createBoardPositions()
+{
+    let x= 0;
+    let y = canvas.height-boardPositionSize;
+    let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1] ;
+
+    for(let i =0 ; i<path.length;i++)
+    {
+
+        if(path[i] == 1)//gaan naar rechts
+        {
+            x = x + 55;
+        }
+        else if(path[i] == 3)//gaan naar links
+        {
+            x = x - 55; 
+        }
+        else if(path[i] == 0)//gaan hier naar boven
+        {
+            y = y - 55; 
+        }
+        boardPositions.push(createRect(x,y,boardPositionSize,boardPositionSize));
+    }
+} 
+
+createBoardPositions();
+draw();
